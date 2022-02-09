52468cf0c9e4d7182b05ccd1d16e536dc86d7e68
Carlos Antonio Ortega Vázquez <99227057+CAntonioOVazquez@users.noreply.github.com>
2022-02-09 14:18:43 -0600

    Add files via upload
    Módulo 1

---
+++ Modulo_01/program01.py
@@ -0,0 +1,57 @@
+# program.py
+sum = 1 + 2
+print(sum)
+
+#Parte 02 ____________________________________________________________________________________________________
+print('Hola desde la consola')
+
+#Parte 03 ____________________________________________________________________________________________________
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+
+#Parte 04 ____________________________________________________________________________________________________
+planetas_en_el_sistema_solar = 8 # int, plutón era considerado un planeta pero ya es muy pequeño
+distancia_a_alfa_centauri = 4.367 # float, años luz
+puede_despegar = True
+transbordador_que_aterrizo_en_la_luna = "Apollo 11" #string
+
+#Parte 05 ____________________________________________________________________________________________________
+# Declaramos la variable
+distancia_a_alfa_centauri = 4.367
+
+# Descubrimos su tipo de dato
+type(distancia_a_alfa_centauri)
+
+#Parte 06 ____________________________________________________________________________________________________
+left_side = 10
+right_side = 5
+left_side / right_side # 2
+
+#Parte 07 ____________________________________________________________________________________________________
+# Importamos la biblioteca 
+from datetime import date
+
+# Obtenemos la fecha de hoy
+date.today()
+
+# Mostramos la fecha en la consola
+print(date.today())
+
+#Parte 08 ____________________________________________________________________________________________________
+print("Today's date is: " + str(date.today()))
+
+#Parte 09 ____________________________________________________________________________________________________
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
+#Parte 10 ____________________________________________________________________________________________________
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(first_number + second_number)
+
+#Parte 11 ____________________________________________________________________________________________________
+print(int(first_number) + int(second_number))
+
